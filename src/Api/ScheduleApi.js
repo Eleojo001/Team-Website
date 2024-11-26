c36e4405614a076b46f9498e5431ab7b5814d2aa
Eleojo001 <yakubujosepheleojojoel@gmail.com>
2024-11-26 21:41:42 +0100

    Result updated

--- src/Api/ScheduleApi.js
+++ src/Api/ScheduleApi.js
@@ -15,43 +15,19 @@ export const ScheduleData = {
             homeTeam: 'PlateauJet SC',  
             awayTeam: 'AS Roma FA',  
             date: '2024-10-30' // Added date  
-        },  
-        {  
-            time: '5:00pm',  
-            venue: 'Central Park',  
-            type: 'N-YOUTH League Cup',  
-            homeTeam: 'Mountain FC',  
-            awayTeam: 'PlateauJet SC',  
-            date: '2024-11-02' // Added date  
-        },  
-        {  
-            time: '6:00pm',  
-            venue: 'Zaria Road Stadium',  
-            type: 'N-YOUTH League Cup',  
-            homeTeam: 'PlateauJet SC',  
-            awayTeam: 'Rangers FC',  
-            date: '2024-11-06' // Added date  
-        },  
-        {  
-            time: '7:00pm',  
-            venue: 'Highland Stadium',  
-            type: 'Playoff',  
-            homeTeam: 'Southern SC',  
-            awayTeam: 'PlateauJet SC',  
-            date: '2024-11-09' // Added date  
-        }  
+        }
     ],  
     results: [  
         {  
             homeTeam: 'PlateauJet SC',  
-            awayTeam: 'City FC',  
+            awayTeam: 'Kano Pillars FC',  
             homeScore: 2,  
-            awayScore: 2,  
+            awayScore: 1,  
             date: '2024-10-01', // Added date  
             goals: [  
                 {  
                     scorer: 'Joseph Yakubu',  
-                    assist: 'James Okwun',  
+                    assist: 'Joseph Yakubu',  
                     time: '30'  
                 },  
                 {  
@@ -65,105 +41,8 @@ export const ScheduleData = {
                     scorer: 'Chinedu Eze',  
                     assist: 'Emmanuel Okafor',  
                     time: '15'  
-                },  
-                {  
-                    scorer: 'Omar Salami',  
-                    assist: 'Nasir Ibrahim',  
-                    time: '70'  
-                }  
+                }, 
             ]  
-        },  
-        {  
-            homeTeam: 'PlateauJet SC',  
-            awayTeam: 'Mountain FC',  
-            homeScore: 3,  
-            awayScore: 1,  
-            date: '2024-10-01', // Added date  
-            goals: [  
-                {  
-                    scorer: 'Mumbarak Mohamed',  
-                    assist: 'Abdul Alonso',  
-                    time: '10'  
-                },  
-                {  
-                    scorer: 'Isekgok Yakubu',  
-                    assist: 'Emmanuel Azizi',  
-                    time: '55'  
-                },  
-                {  
-                    scorer: 'Ali Idris',  
-                    assist: 'Paul Ijedinma',  
-                    time: '80'  
-                }  
-            ],  
-            awayGoals: [  
-                {  
-                    scorer: 'Emmanuel Nwafor',  
-                    assist: 'George Okoro',  
-                    time: '80'  
-                }  
-            ]  
-        },  
-        {  
-            homeTeam: 'PlateauJet SC',  
-            awayTeam: 'Rangers FC',  
-            homeScore: 1,  
-            awayScore: 3,  
-            date: '2024-10-01', // Added date  
-            goals: [  
-                {  
-                    scorer: 'Ali Idris',  
-                    assist: 'Paul Ijedinma',  
-                    time: '45'  
-                }  
-            ],  
-            awayGoals: [  
-                {  
-                    scorer: 'Chukwudi Anwuka',  
-                    assist: 'Kingsley Uzo',  
-                    time: '20'  
-                },  
-                {  
-                    scorer: 'Obinna Nze',  
-                    assist: 'Emeka Ukwu',  
-                    time: '35'  
-                },  
-                {  
-                    scorer: 'David Ogbodo',  
-                    assist: 'Samuel Dike',  
-                    time: '90'  
-                }  
-            ]  
-        },  
-        {  
-            homeTeam: 'PlateauJet SC',  
-            awayTeam: 'Southern SC',  
-            homeScore: 4,  
-            awayScore: 0,  
-            date: '2024-11-01', // Added date  
-            goals: [  
-                {  
-                    scorer: 'Emeka Chukwu',  
-                    assist: 'Kola Ajayi',  
-                    time: '12'  
-                },  
-                {  
-                    scorer: 'Ngozi Eze',  
-                    assist: 'Tunde Adedoyin',  
-                    time: '35'  
-                },  
-                {  
-                    scorer: 'Ifeoma Ukwu',  
-                    assist: 'Chinonso Eze',  
-                    time: '50'  
-                },  
-                {  
-                    scorer: 'Odion Obiorah',  
-                    assist: 'Solomon Nwodo',  
-                    time: '88'  
-                }  
-            ],  
-            awayGoals: [] // No away goals  
-        }  
+        },
     ]  
 };
